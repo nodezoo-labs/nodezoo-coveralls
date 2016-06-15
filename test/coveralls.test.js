3114c0568c5e107d1ddb87c0b66fba9892e1f747
Mihai Dima <mihaizn@gmail.com>
2016-06-15 15:20:34 +0300

    test isolation

--- test/coveralls.test.js
+++ test/coveralls.test.js
@@ -12,40 +12,18 @@ const InvalidPluginName = 'qwerty_qwerty'
 
 const lab = exports.lab = Lab.script()
 const describe = lab.describe
-const suite = lab.suite
 const before = lab.before
 const it = lab.it
 const expect = Code.expect
 
 const NpmRegistry = 'http://registry.npmjs.org/'
 
-const RequestMap = [
-  {
-    urlMatch: NpmRegistry + 'seneca',
-    err: null,
-    response: {},
-    body: JSON.stringify(NpmSenecaFakeData)
-  },
-  {
-    urlMatch: InvalidPluginName,
-    err: null,
-    response: {},
-    body: '{}'
-  },
-  {
-    urlMatch: 'coveralls.io',
-    err: null,
-    response: {},
-    body: JSON.stringify(CoverallsSenecaFakeData)
-  }
-]
+var RequestMap = []
 
 const RequestProxy = {
   request: {
     get: (opts, done) => {
-      const request = _.find(RequestMap, (o) => {
-        return opts.url.includes(o.urlMatch)
-      })
+      const request = _.find(RequestMap, o => opts.url.includes(o.urlMatch))
       if (request) {
         done(request.err, request.response, request.body)
       }
@@ -71,7 +49,22 @@ process.setMaxListeners(12)
 
 const si = createInstance()
 
-suite('nodezoo-coveralls suite tests ', function () {
+describe('nodezoo-coveralls suite tests ', function () {
+  const DefaultRequestMap = [
+    {
+      urlMatch: NpmRegistry + 'seneca',
+      err: null,
+      response: {},
+      body: JSON.stringify(NpmSenecaFakeData)
+    },
+    {
+      urlMatch: 'coveralls.io',
+      err: null,
+      response: {},
+      body: JSON.stringify(CoverallsSenecaFakeData)
+    }
+  ]
+
   before({}, function (done) {
     si.ready(function (err) {
       if (err) {
@@ -80,112 +73,125 @@ suite('nodezoo-coveralls suite tests ', function () {
       done()
     })
   })
-})
 
-describe('Valid "role:coveralls,cmd:get" calls', () => {
-  it('test non cached valid response', function (done) {
-    const payload = { 'name': 'seneca' }
-    si.act('role:coveralls,cmd:get', payload, function (err, reply) {
-      expect(err).to.not.exist()
-      expect(reply.ok).to.be.true()
-      expect(reply.data.name).to.equal(payload.name)
-      expect(reply.data.coveredPercent).to.be.a.number()
-      expect(reply.data.coverageChange).to.be.a.number()
-      expect(reply.data.badgeUrl).to.exist()
-      expect(reply.data.cached).to.exist()
+  describe('Valid "role:coveralls,cmd:get" calls', () => {
+    it('test non cached valid response', function (done) {
+      const payload = { 'name': 'seneca' }
+      RequestMap = DefaultRequestMap.slice()
+      si.act('role:coveralls,cmd:get', payload, function (err, reply) {
+        expect(err).to.not.exist()
+        expect(reply.ok).to.be.true()
+        expect(reply.data.name).to.equal(payload.name)
+        expect(reply.data.coveredPercent).to.be.a.number()
+        expect(reply.data.coverageChange).to.be.a.number()
+        expect(reply.data.badgeUrl).to.exist()
+        expect(reply.data.cached).to.exist()
 
-      done(err)
+        done(err)
+      })
     })
-  })
 
-  it('test cached valid response', function (done) {
-    const payload = { 'name': 'seneca' }
-    si.act('role:coveralls,cmd:get', payload, function (err, reply) {
-      expect(err).to.not.exist()
+    it('test cached valid response', function (done) {
+      const payload = { 'name': 'seneca' }
+      RequestMap = DefaultRequestMap.slice()
 
-      const cachedOne = reply.cached
+      si.act('role:coveralls,cmd:get', payload, function (err, reply) {
+        expect(err).to.not.exist()
+
+        const cachedOne = reply.cached
+
+        si.act('role:coveralls,cmd:get', payload, (err, reply) => {
+          expect(err).to.not.exist()
+
+          const cachedTwo = reply.cached
+
+          expect(cachedOne).to.equal(cachedTwo)
+          done()
+        })
+      })
+    })
+
+    it('test non-cached valid reponse - update flag', (done) => {
+      const payload = {name: 'seneca'}
+      RequestMap = DefaultRequestMap.slice()
 
       si.act('role:coveralls,cmd:get', payload, (err, reply) => {
         expect(err).to.not.exist()
 
-        const cachedTwo = reply.cached
+        const cachedOne = reply.data.cached
+        payload.update = true
 
-        expect(cachedOne).to.equal(cachedTwo)
-        done()
+        si.act('role:coveralls,cmd:get', payload, (err, reply) => {
+          expect(err).to.not.exist()
+
+          const cachedTwo = reply.data.cached
+
+          expect(cachedOne).to.be.below(cachedTwo)
+
+          done()
+        })
       })
     })
   })
 
-  it('test non-cached valid reponse - update flag', (done) => {
-    const payload = {name: 'seneca'}
+  describe('An invalid "role:coveralls,cmd:get" call', () => {
+    it('has an error and no data', (done) => {
+      const seneca = createInstance()
+      const payload = {name: InvalidPluginName}
+      const invalidPluginMap = {
+        urlMatch: InvalidPluginName,
+        err: null,
+        response: {},
+        body: '{}'
+      }
 
-    si.act('role:coveralls,cmd:get', payload, (err, reply) => {
-      expect(err).to.not.exist()
+      seneca.ready(function () {
+        RequestMap = _.concat(DefaultRequestMap, invalidPluginMap)
+        seneca.act('role:coveralls,cmd:get', payload, (err, reply) => {
+          expect(err).to.not.exist()
+          expect(reply.ok).to.be.false()
+          expect(reply.err).to.exist()
 
-      const cachedOne = reply.data.cached
-      payload.update = true
-
-      si.act('role:coveralls,cmd:get', payload, (err, reply) => {
-        expect(err).to.not.exist()
-
-        const cachedTwo = reply.data.cached
-
-        expect(cachedOne).to.be.below(cachedTwo)
-
-        done()
-      })
-    })
-  })
-})
-
-describe('An invalid "role:coveralls,cmd:get" call', () => {
-  it('has an error and no data', (done) => {
-    const seneca = createInstance()
-    const payload = {name: InvalidPluginName}
-
-    seneca.ready(function () {
-      seneca.act('role:coveralls,cmd:get', payload, (err, reply) => {
-        expect(err).to.not.exist()
-        expect(reply.ok).to.be.false()
-        expect(reply.err).to.exist()
-
-        done()
-      })
-    })
-  })
-})
-
-describe('A valid "role:info,req:part" call', () => {
-  it('has no error and has data', (done) => {
-    const seneca = createInstance()
-    const payload = {name: 'seneca'}
-
-    seneca.ready(function () {
-      seneca.act('role:info,req:part', payload, (err, reply) => {
-        expect(err).to.not.exist()
-        expect(reply.ok).to.be.true()
-
-        done()
+          done()
+        })
       })
     })
   })
 
-  it('responds via "role:info,res:part"', (done) => {
-    const seneca = createInstance()
-    const payload = {name: 'seneca'}
+  describe('Valid "role:info,req:part" calls', () => {
+    it('has no error and has data', (done) => {
+      const seneca = createInstance()
+      const payload = {name: 'seneca'}
 
-    seneca.ready(function () {
-      seneca.add('role:info,res:part', (msg, cb) => {
-        expect(msg).to.exist()
+      seneca.ready(function () {
+        RequestMap = DefaultRequestMap.slice()
+        seneca.act('role:info,req:part', payload, (err, reply) => {
+          expect(err).to.not.exist()
+          expect(reply.ok).to.be.true()
 
-        cb()
-        done()
+          done()
+        })
       })
+    })
 
-      seneca.act('role:info,req:part', payload, (err, reply) => {
-        expect(err).to.not.exist()
-        expect(reply).to.exist()
-        expect(reply.ok).to.be.true()
+    it('responds via "role:info,res:part"', (done) => {
+      const seneca = createInstance()
+      const payload = {name: 'seneca'}
+
+      seneca.ready(function () {
+        RequestMap = DefaultRequestMap.slice()
+        seneca.add('role:info,res:part', (msg, cb) => {
+          expect(msg).to.exist()
+
+          cb()
+          done()
+        })
+
+        seneca.act('role:info,req:part', payload, (err, reply) => {
+          expect(err).to.not.exist()
+          expect(reply).to.exist()
+          expect(reply.ok).to.be.true()
+        })
       })
     })
   })
